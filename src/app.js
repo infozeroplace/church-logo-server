0f74d6cb6ad4a874c00388262fe15bf5c0fa6aaa
Ruman-Islam <rumanislam48@gmail.com>
2024-12-05 16:50:49 +0600

    done

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(
   (req, res, next) => {
     if (req.originalUrl.includes("/payment/webhook")) {
-      // req.rawBody = req.body;
+      req.rawBody = req.body;
       next(); // Skip JSON parsing for this route
     } else {
       express.json({
