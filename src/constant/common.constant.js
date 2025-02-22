b410767b1e19ce0051fc750cda85efb1ae50178d
Ruman-Islam <rumanislam48@gmail.com>
2025-02-23 01:23:28 +0600

    fixing

--- src/constant/common.constant.js
+++ src/constant/common.constant.js
@@ -1,7 +1,10 @@
 import config from "../config/index.js";
 
 export const corsOptions = {
-  origin: config.origins,
+  origin: [
+    ...config.origins,
+    "mongodb://churchlogo.info%40gmail.com:churchlogo123456aA%40@185.201.9.31:27017/church_logo",
+  ],
   optionsSuccessStatus: 200,
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
