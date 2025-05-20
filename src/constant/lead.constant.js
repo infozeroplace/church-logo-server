6026830bc4903f72070298a09767dba9cb2cc8f8
Ruman-Islam <rumanislam48@gmail.com>
2025-05-20 10:50:38 +0600

    mod: email lowercase

--- src/constant/lead.constant.js
+++ src/constant/lead.constant.js
@@ -1,11 +1,8 @@
-export const leadSearchableFields = [
-  "city",
-  "countryName",
-  "continentName"
-];
+export const leadSearchableFields = ["city", "countryName", "continentName"];
 
 export const leadFilterableField = [
+  "searchTerm",
   "city",
   "countryName",
-  "continentName"
+  "continentName",
 ];
